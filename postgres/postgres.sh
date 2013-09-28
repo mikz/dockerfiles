dee8700826949080f669cb02be65a671ccdfa2b4
Michal Cichra <mikz@r2d2.o2h.cz>
2013-09-28 13:47:26 +0200

    import dockerfiles

---
+++ postgres/postgres.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+PARAMS="${@}"
+
+if [ -z "$PG_VERSION" ]
+  then
+    echo "No pg version supplied"
+    exit 1
+fi
+
+PG_PATH="/usr/lib/postgresql/${PG_VERSION}/bin/"
+
+if [ -z "$1" ]
+  then
+    echo "No argument supplied. Use one of:"
+    ls ${PG_PATH}
+    exit 1
+fi
+
+CMD="${PG_PATH}${PARAMS}"
+echo "RUN: ${CMD}"
+chown postgres:postgres "$PGDATA"
+su postgres -c "${CMD}"
